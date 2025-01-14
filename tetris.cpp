c9bfa3b055162afd5518f0523dbdcf2ab0cea65d
Your Name <you@example.com>
2025-01-14 15:44:28 +0100

    music

--- tetris.cpp
+++ tetris.cpp
@@ -2,9 +2,39 @@
 #include <vector>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
-int main {
+int main() {
 
-    sf::Window window()
+    sf::RenderWindow window(sf::VideoMode(800,600), "Tetris");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+
+            sf::SoundBuffer buffer;
+    if (!buffer.loadFromFile("Tetris.wav")) {
+        std::cerr << "Failed to load .wav file!" << std::endl;
+        return -1;
+    }
+
+        // Create a sound object and set its buffer
+        sf::Sound sound;
+        sound.setBuffer(buffer);
+
+        // Play the sound
+        sound.play();
+        // Effacer l'écran
+        window.clear(sf::Color::Black);
+        // Afficher le contenu
+        window.display();
+    }
+    
+
 
 }
